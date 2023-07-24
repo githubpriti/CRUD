b40da8b91efbe3398fea0e67476116f3bbe02f9b
Vaishnavi jaju <jajuvh@gmail.com>
2023-07-24 12:23:14 +0530

    server.js file updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const app = require("./app");      //imported app here
 const PORT = process.env.PORT || 4000;  //means if process.env not executed thn 4000 will execute.
 
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
     console.log(`App is running at http://localhost:${PORT}`);
 });
