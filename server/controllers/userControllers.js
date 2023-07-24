b48b9812822770e1cba95d662fe2dfc5a495cbf8
Vaishnavi jaju <jajuvh@gmail.com>
2023-07-24 15:44:37 +0530

    updated files

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -36,7 +36,7 @@ exports.createUser = async (req, res) => {
   }
 };
 
-exports.getUser = async (req, res) => {
+exports.getUsers = async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json({
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,11 @@
 // URL PATH
 const express = require("express");
-const {home, createUser, getUser, editUser, deleteUser,} = require("../controllers/userControllers");
+const {home, createUser, getUsers, editUser, deleteUser,} = require("../controllers/userControllers");
 const router = express.Router();
 
 router.get("/", home );
 router.post("/createUser", createUser);
-router.get("/getUser", getUser);
+router.get("/getUsers", getUsers);
 router.put("/editUser/:id", editUser);       //edit means put this value nd we need id also of which we want to edit n update.
 router.delete("/deleteUser/:id", deleteUser);
 
